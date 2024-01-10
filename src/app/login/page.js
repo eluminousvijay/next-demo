a4e3cf7cc5fda9c8140217efaa9848e8918a2520
eluminousvijay <eluminous_se88@eluminoustechnologies.com>
2024-01-10 18:55:22 +0530

    change: footer changes

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -9,6 +9,32 @@ const YourMainPage = () => {
     <main className={styles.main}>
       <Header />
       <Login />
+      <div className={styles.achievements}>
+        <div className={styles.work}>
+          <i className="fas fa-atom" />
+          <p className={styles.work_heading}>Projects</p>
+          <p className={styles.work_text}>
+            Explore the exciting projects I've worked on. Each project is a
+            story of creativity and problem-solving.
+          </p>
+        </div>
+        <div className={styles.work}>
+          <i className="fas fa-skiing" />
+          <p className={styles.work_heading}>Skills</p>
+          <p className={styles.work_text}>
+            I possess a diverse skill set, including Vue JS, React JS, Next JS,
+            Nuxt JS, Laravel, and more.
+          </p>
+        </div>
+        <div className={styles.work}>
+          <i className="fas fa-ethernet" />
+          <p className={styles.work_heading}>Network</p>
+          <p className={styles.work_text}>
+            Networking is not just about computers; it's about building
+            connections. Let's connect and grow together.
+          </p>
+        </div>
+      </div>
       <Footer />
     </main>
   );
