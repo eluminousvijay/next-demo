fddca6579487bbc34b9bb083163330335e482378
eluminousvijay <eluminous_se88@eluminoustechnologies.com>
2024-01-12 12:44:11 +0530

    change: changes

--- src/app/clientComponent/page.js
+++ src/app/clientComponent/page.js
@@ -1,21 +1,21 @@
-// "use client";
-// import { useRef } from "react";
-// import { useAppSelector,  useAppStore } from "../../lib/hooks";
+"use client";
+import { useRef } from "react";
+import { useAppSelector,  useAppStore } from "../../lib/hooks";
 
-// export default function ProductName({ product }) {
-//   const store = useAppStore();
+export default function ProductName({ product }) {
+  const store = useAppStore();
 
-//   const name = useAppSelector(
-//     (state) => state.product?.productData?.productName
-//   );
+  const name = useAppSelector(
+    (state) => state.product?.productData?.productName
+  );
 
-//   console.log(">>>>>>>>>>>>", name);
+  console.log(">>>>>>>>>>>>", name);
 
-//   return (
-//     <>{name}</>
-//     // <input
-//     //   value={name}
-//     //   onChange={(e) => dispatch(setProductName(e.target.value))}
-//     // />
-//   );
-// }
+  return (
+    <>{name}</>
+    // <input
+    //   value={name}
+    //   onChange={(e) => dispatch(setProductName(e.target.value))}
+    // />
+  );
+}
