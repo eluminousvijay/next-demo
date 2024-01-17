38067750a53ccb8b8609bf109102bebe1cf74cc7
eluminousvijay <eluminous_se88@eluminoustechnologies.com>
2024-01-17 12:51:08 +0530

    change : DB changes

--- src/app/api/backed/route.js
+++ src/app/api/backed/route.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 // const API_BASE_URL = "http://localhost:9000";
 const API_BASE_URL = "https://kra.betaeserver.com";
+const API_BASE_URL_OTHER = "https://developervijay.vercel.app/";
 
 const axiosInstance = axios.create({
   baseURL: API_BASE_URL,
@@ -11,10 +12,23 @@ export const setUserToken = (token) => {
   axiosInstance.defaults.headers.common["Authorization"] = `Bearer ${token}`;
 };
 
-export const loginUser = async (config = {}) => {
+export const loginUser = async (formData) => {
   try {
-    const response = await axiosInstance.post("/api/user/login", config);
-    return response.data;
+    // const response = await axiosInstance.post("/api/user/login", config);
+    // return response.data;
+    const res = await fetch(API_BASE_URL_OTHER + "api/db/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    });
+
+    if (!res.ok) {
+      throw new Error("Failed to send data");
+    }
+
+    return res.json();
   } catch (error) {
     throw error;
   }
---
+++ src/app/api/db/login/route.js
@@ -0,0 +1,46 @@
+import { sql } from "@vercel/postgres";
+import { NextResponse } from "next/server";
+
+export async function POST(request) {
+  try {
+    const { username, password } = await request.json();
+    if (!username || !password) {
+      throw new Error("Username and password are required");
+    }
+
+    const result = await sql`
+      SELECT * FROM Users
+      WHERE name = ${username} AND password = ${password};
+    `;
+
+    if (result.rows.length > 0) {
+      const user = result.rows[0];
+      const token = generateToken(16);
+
+      await sql`
+        UPDATE Users
+        SET token = ${token}
+        WHERE user_id = ${user.user_id};
+      `;
+
+      return NextResponse.json({ token }, { status: 200 });
+    } else {
+      throw new Error("Invalid credentials");
+    }
+  } catch (error) {
+    return NextResponse.json({ error: error.message }, { status: 401 });
+  }
+}
+
+function generateToken(length) {
+  const characters =
+    "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+  let token = "";
+
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    token += characters.charAt(randomIndex);
+  }
+
+  return token;
+}
