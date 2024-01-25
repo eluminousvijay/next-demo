6bc834078fc6718630cf2f5095c9c02b01c27b67
eluminousvijay <eluminous_se88@eluminoustechnologies.com>
2024-01-25 16:32:48 +0530

    changes: middleware changes

--- src/middleware.js
+++ src/middleware.js
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 
 export function middleware(request) {
   const cookie = request.cookies.get("token");
-  const isUserLoggedIn = cookie.value ? true : false;
+  const isUserLoggedIn = cookie?.value ? true : false;
 
   if (request.nextUrl.pathname.startsWith("/user") && !isUserLoggedIn) {
     return NextResponse.redirect(new URL("/login", request.url));
